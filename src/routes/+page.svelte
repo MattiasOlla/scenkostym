41f240daed867190d307f6776dc656272ad658fe
Mattias <mattias.olla@gmail.com>
2025-06-06 09:56:35 +0200

    Make it clearer if we're in dev or not

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import ProfileCard from "$components/ProfileCard.svelte";
   import { people } from "$lib/people";
+
+  import { page } from "$app/state";
+  const titleSuffix = ["localhost", "127.0.0.1"].includes(page.url.hostname) ? " (DEV)" : "";
 </script>
 
 <svelte:head>
-  <title>Scenkostym 2025</title>
+  <title>Scenkostym 2025{titleSuffix}</title>
 </svelte:head>
 
 <h1 class="page-title">Scenkostym och historiska dräkter</h1>
